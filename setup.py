a24541c27d132817d65686ef60a280267f826430
Alen Siljak <alen.siljak@gmx.com>
2019-03-05 13:39:44 +0100

    v1.2.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ Setup configuration for the package.
 from setuptools import setup
 
 setup(name='finance_quote_python',
-      version='1.1',
+      version='1.2.0',
       description='Finance::Quote implementation in Python',
       url='https://github.com/MisterY/finance-quote-python',
       author='Alen Siljak',
